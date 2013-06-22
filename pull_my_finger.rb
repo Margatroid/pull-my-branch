659f72e103e83494eb2048a0ac2506c4d328e3e3
Alvis Duong <alvisduong@gmail.com>
2013-06-22 16:09:46 +0100

    Create new classes to hold views and app

--- pull_my_finger.rb
+++ pull_my_finger.rb
@@ -1,5 +1,25 @@
 require 'sinatra'
 
-get '/' do
-  'Hello World!'
+class Views
+  attr_reader :index
+
+  def initialize
+    @index = <<ERB
+      <!DOCTYPE html>
+      <html lang="en">
+        <head><title>Pull my branch</title></head>
+        <body>Hello world</body>
+      </html>
+ERB
+  end
 end
+
+class App < Sinatra::Base
+  views = Views.new()
+
+  get '/' do
+    erb views.index
+  end
+end
+
+App.run!
